c4b334c0505428fb4afd870443c23deedc1f397b
Arlene George <aemg@live.com>
2019-08-21 16:08:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,8 @@ def load_library(emoticon_file)
     english = e[0]
     japanese = e[1]
     
-    japanese_emoji_library['get_meaning'][japanese]
+    japanese_emoji_library['get_meaning'][japanese] = meaning
+    japanese_emoji_library[]
 end
 
 
