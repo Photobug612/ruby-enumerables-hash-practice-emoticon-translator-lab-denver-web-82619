5bb5bd2fa356d867123a5cb99b26a4d4b3cbdb6d
Arlene George <aemg@live.com>
2019-08-21 16:05:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,6 +3,7 @@ def load_library(emoticon_file)
   japanese_emoji = YAML.load(File.read("lib/emoticons.yml"))
     japanese_emoji_library = {'get_meaning'  => {},
                   'get_emoticon' => {} }
+  japanese_emoji.each do |
 end
 
 
