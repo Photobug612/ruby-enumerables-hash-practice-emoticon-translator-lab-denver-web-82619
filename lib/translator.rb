665d1ff6823cd54f0c7c8ffea2cdc70e29bc2faf
Arlene George <aemg@live.com>
2019-08-21 16:33:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ end
 
 def get_japanese_emoticon(emoticon_file, emoticon)
   emoticons = load_library(emoticon_file)
-  japanese_emoticons = emoticons['emoticon'][emoticon]
+  japanese_emoticons = emoticons['get_emoticon'][emoticon]
 end
 
 def get_english_meaning
